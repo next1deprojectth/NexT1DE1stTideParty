c1ff75c3535599db7ae018a3287439a0b0e7bd6f
thanyada <thanyada.c@proxumer.com>
2026-03-23 20:02:56 +0700

    close workshop

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 const API_CONFIG = {
     BASE_URL: "https://script.google.com/macros/s/AKfycbwOsXpCe7YgLGxY7sIL4kxV9sMGGH5YFpoXrZbnm0LJwpGG1zzUUduUunro-s8xzmrNmQ/exec",
     IS_DONATE_OPEN: true,
-    IS_WORKSHOP_OPEN: true
+    IS_WORKSHOP_OPEN: false
 };
