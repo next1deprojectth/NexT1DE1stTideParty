7085da27ef9176ca64b81331615ad26b0c7dff46
thanyada <thanyada.c@proxumer.com>
2026-03-25 18:16:35 +0700

    chore: Update production API BASE_URL in config.js.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Centralized configuration for the project
 const API_CONFIG = {
     // Production
-    BASE_URL: "https://script.google.com/macros/s/AKfycbwOsXpCe7YgLGxY7sIL4kxV9sMGGH5YFpoXrZbnm0LJwpGG1zzUUduUunro-s8xzmrNmQ/exec",
+    BASE_URL: "https://script.google.com/macros/s/AKfycbxcIbbCVs2Di3bbN1UJOIg1YWNfH_57VKur1zN0cR1F4WR1k__FnFwQPzuSP4Xv9usDTg/exec",
     // SIT
     // BASE_URL: "https://script.google.com/macros/s/AKfycbx8En3TTGuvHtKWc0kegthD3bvZi6RiqgiiKDempJdudaBac992iU4JjD3Vp5vAI3qU5A/exec",
     IS_DONATE_OPEN: true,
